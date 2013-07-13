41391ed04e9ce26974f14660b1b68d00707e64fc
Andrei Nikolov <andrei.nikolov@gmail.com>
2013-07-13 17:17:22 +0000

    Grader now accepts URL

--- grader.js
+++ grader.js
@@ -23,16 +23,42 @@ var loadChecks = function(checksfile) {
     return JSON.parse(fs.readFileSync(checksfile));
 };
 
-var checkHtmlFile = function(htmlfile, checksfile) {
-    $ = cheerioHtmlFile(htmlfile);
-    var checks = loadChecks(checksfile).sort();
-    var out = {};
-    for (var ii in checks) {
-	var present = $(checks[ii]).length > 0;
-	out[checks[ii]] = present;
+var checkHtmlFile = function(htmlfile, url, checksfile) {
+
+    var finishWork = function() {
+	var checks = loadChecks(checksfile).sort();
+	var out = {};
+	for (var ii in checks) {
+	    var present = $(checks[ii]).length > 0;
+	    out[checks[ii]] = present;
+	    
+	}
 	
+	var outJson = JSON.stringify(out, null, 4);
+	console.log(outJson);
+
     }
-    return out;
+
+    if (url)
+    {
+	var restler = require("restler");
+	restler.get(url).on('complete', function(data, response) {
+	    if (!response || response.statusCode != 200) {
+		console.log("%s cannot be loaded", url);
+		process.exit(1);
+	    }
+	    else {
+		$ = cheerio.load(data);
+		finishWork();
+	    }
+	});
+    }
+    else
+    {
+	$ = cheerioHtmlFile(htmlfile);
+	var ret = finishWork();
+    }
+
 };
 
 var clone = function(fn) {
@@ -42,11 +68,22 @@ var clone = function(fn) {
 if (require.main == module) {
     program
 	.option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
-	.option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+	.option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), '')
+	.option('-u, --url <url>', 'URL to index.html', '')
 	.parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+
+    if (program.url && program.file)
+    {
+	console.log("Please provide File OR URL, not both");
+	process.exit(1);
+    }
+
+    if (!(program.url || program.file))
+    {
+	program.file = HTMLFILE_DEFAULT;
+    }
+
+    checkHtmlFile(program.file, program.url, program.checks);
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
